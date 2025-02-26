127d37b9d769a45751ca1b616720a83da02523de
Moamen Ghareeb <moamen_ghareeb@icloud.com>
2025-02-26 04:00:49 +0200

    Fix dependencies and build configuration

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
+  trailingSlash: true,
+  poweredByHeader: false,
+  productionBrowserSourceMaps: false,
+  // Workaround for NFT issue
+  experimental: {
+    outputFileTracingRoot: process.env.NODE_ENV === 'production' ? undefined : __dirname,
+  },
 }
 
 module.exports = nextConfig 
