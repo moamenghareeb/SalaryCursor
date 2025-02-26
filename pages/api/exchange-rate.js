60a61be7004a52f83e7f619ac869174cda048ba6
Moamen Ghareeb <moamen_ghareeb@icloud.com>
2025-02-26 06:02:32 +0200

    Fix exchange rate API with hardcoded value

--- pages/api/exchange-rate.js
+++ pages/api/exchange-rate.js
@@ -1,7 +1,3 @@
-export const config = {
-  runtime: 'edge'
-};
-
 export default function handler(req, res) {
   // Return a hardcoded exchange rate for testing
   res.status(200).json({ exchangeRate: 31.5 });
