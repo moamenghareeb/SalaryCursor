129d2b0449478583dadd6d7767cd3557335b3a4a
Moamen Ghareeb <moamen_ghareeb@icloud.com>
2025-02-26 06:09:29 +0200

    Update exchange rate to current market value (50.3)

--- pages/api/exchange-rate.js
+++ pages/api/exchange-rate.js
@@ -1,8 +1,8 @@
 // Simple, reliable static response
 export default function handler(req, res) {
-  // Return exchange rate hardcoded
+  // Return exchange rate hardcoded with accurate value
   return res.status(200).json({
-    exchangeRate: 31.5,
+    exchangeRate: 50.3,
     source: 'static'
   });
 } 
